5956b5bee53e3d241603f353d6993363788a5fe5
Dongwoo Kim <kimdwkimdw@gmail.com>
2012-12-17 02:22:45 +0900

    model added

---
+++ js/backbone.modules/irc.talk.js
@@ -0,0 +1,20 @@
+var ServerModel = Backbone.Model.extend({
+  /*Member*/
+  //channelList
+});
+
+var ServerCollection = Backbone.Collection.extend({
+  model: ServerModel
+});
+
+var ChannelModel = Backbone.Model.extend({
+
+});
+
+var ChannelCollection = Backbone.Collection.extend({
+  model: ChannelModel
+});
+
+
+//side server list view
+  //foreach { channel list view }
